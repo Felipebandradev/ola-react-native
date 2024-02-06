59fc05cc4573c10f987ae13608fd932d24cdf750
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-02-06 10:14:54 -0300

    feat: alteração do texto inicial

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá React Native e Expo! 👨‍💻</Text>
       <StatusBar style="auto" />
     </View>
   );
