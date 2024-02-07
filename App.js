3e578c13b70b9b0e76a877acb05747c58ea0f51f
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-02-07 11:12:16 -0300

    feat: flex

--- App.js
+++ App.js
@@ -3,9 +3,35 @@ import {
   Button,
   SafeAreaView,
   StatusBar,
+  StyleSheet,
   Text,
   View,
 } from "react-native";
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "#ebe7f8",
+    flex: 1,
+    // justifyContent: "center"
+  },
+
+  cabecalho: {
+    backgroundColor: "#aba2e3",
+    padding: 10,
+    flex: 0.6,
+
+  },
+
+  conteudo: {
+    backgroundColor: "#c1b9ea",
+    flex: 7,
+  },
+
+  rodape: {
+    backgroundColor: "#d6d0f1",
+    flex: 1,
+  },
+});
 
 export default function App() {
   const vai = () => {
@@ -16,14 +42,14 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="default" />
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.cabecalho}>
           <Text>Topo/Cabecalho</Text>
         </View>
-        <View>
+        <View style={estilos.conteudo}>
           <Text>Conteúdo...</Text>
         </View>
-        <View>
+        <View style={estilos.rodape}>
           <Text>Rodapé..</Text>
           <Button title="Toca Aqui" onPress={vai} />
         </View>
